dc632ddb4aa11265d6e1f056ca14685d9ada224f
aindreboe <aindreboe@gmail.com>
2018-11-02 13:48:17 +0000

    flatten i rekursiv

---
+++ IterativeListManipulator.java
@@ -0,0 +1,378 @@
+package impl;
+
+import java.util.Comparator;
+import java.util.HashSet;
+
+import common.InvalidIndexException;
+import common.ListNode;
+import interfaces.IListManipulator;
+import interfaces.IOperator;
+import interfaces.ITransformation;
+
+/**
+ * This class represents the iterative implementation of the IListManipulator interface.
+ *
+ */
+public class IterativeListManipulator implements IListManipulator {
+
+    @Override
+    public int size(ListNode head) { //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+        int size = 0;
+
+        ListNode current = head;
+
+        if (head == null) {
+            return 0;
+        }
+
+        do {
+            size++;
+            current = current.next;
+        } while (current != null);
+
+        return size;
+    }
+
+    @Override
+    public boolean contains(ListNode head, Object element) {
+
+        if (head == null) {
+            return false;
+        }
+
+        ListNode current = head;
+
+        do {
+            if (current.element.equals(element)) {
+                return true;
+            }
+            current = current.next;
+        } while (current != null);
+
+        return false;
+    }
+
+    @Override
+    public int count(ListNode head, Object element) {
+
+        int count = 0;
+
+        if (head == null) {
+            return 0;
+        }
+
+        ListNode current = head;
+
+        do {
+            if (current.element.equals(element)) {
+                count++;
+            }
+            current = current.next;
+        } while (current != null);
+
+        return count;
+    }
+
+    @Override
+    public String convertToString(ListNode head) {
+
+        if (head == null) {
+            return "";
+        }
+
+        StringBuilder builder = new StringBuilder();
+
+        ListNode current = head;
+
+        do {
+
+            if (current != head) {
+                builder.append(",");
+            }
+
+            builder.append(current.element.toString());
+            current = current.next;
+
+        } while (current != null);
+
+        return builder.toString();
+    }
+
+    @Override
+    public Object getFromFront(ListNode head, int n) throws InvalidIndexException {
+
+        ListNode current = head;
+
+        if (head == null) {
+            throw new InvalidIndexException();
+        }
+
+        for (int i = 0; i < n; i++) {
+            current = current.next;
+            if (current == null) {
+                throw new InvalidIndexException();
+            }
+        }
+        return current.element;
+    }
+
+    @Override
+    public Object getFromBack(ListNode head, int n) throws InvalidIndexException {
+
+        int index = size(head) - n - 1;
+
+        if (index < 0) {
+            throw new InvalidIndexException();
+        }
+
+        ListNode current = head;
+
+        if (head == null) {
+            throw new InvalidIndexException();
+        }
+
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+            if (current == null) {
+                throw new InvalidIndexException();
+            }
+        }
+        return current.element;
+    }
+
+    @Override
+    public boolean deepEquals(ListNode head1, ListNode head2) {
+
+        if ((head1 == null && head2 != null) || (head1 != null && head2 == null)) {
+            return false;
+        }
+
+        if (head1 == null && head2 == null) {
+            return true;
+        }
+
+        ListNode current1 = head1;
+        ListNode current2 = head2;
+
+        do {
+
+            if (!current1.element.equals(current2.element)) {
+                return false;
+            }
+
+            current1 = current1.next;
+            current2 = current2.next;
+
+        } while (current1 != null && current2 != null);
+
+        if (current1 != null || current2 != null) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public ListNode deepCopy(ListNode head) {
+
+        if (head == null) {
+            return null;
+        }
+
+        ListNode current = head;
+        ListNode copy = new ListNode(head.element);
+
+        ListNode copyHead = copy;
+
+        do { //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+            if (current.next != null) {
+                copy.next = new ListNode(current.next.element);
+            }
+            copy.element = current.element;
+
+            current = current.next;
+            copy = copy.next;
+
+        } while (current != null);
+
+        return copyHead;
+    }
+
+    @Override
+    public boolean containsDuplicates(ListNode head) {
+        HashSet<Object> set = new HashSet<>();
+
+        if (head == null) {
+            return false;
+        }
+
+        ListNode current = head;
+
+        do {
+            if (set.contains(current.element)) {
+                return true;
+            } else {
+                set.add(current.element);
+            }
+            current = current.next;
+        } while (current != null);
+
+        return false;
+    }
+
+    @Override
+    public ListNode append(ListNode head1, ListNode head2) {
+
+        if (head1 == null) {
+            return head2;
+        } else if (head2 == null) {
+            return head1;
+        }
+
+        ListNode currentHead = head1;
+        ListNode current = currentHead;
+
+        do {
+            current = current.next;
+        } while (current.next != null);
+
+        current.next = head2;
+
+        return currentHead;
+    }
+
+    @Override
+    public ListNode flatten(ListNode head) { //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        if (head == null) {
+            return null;
+        }
+
+        ListNode flatHead = head;
+        ListNode flat = flatHead;
+        ListNode current = head;
+
+        current = current.next;
+
+        while (current != null && current != head) {
+
+            System.out.println("!");
+
+            if (current.element instanceof ListNode) {
+
+                System.out.println("!!!");
+                ListNode tempHead = (ListNode) current.element;
+                flat.next = (ListNode) current.element;
+
+                while (flat.next != null && flat.next != tempHead) {
+                    flat = flat.next;
+                    System.out.println(flat.element);
+                }
+
+            } else {
+                flat.next = current;
+            }
+
+            current = current.next;
+
+            if (current != null) {
+
+                flat = flat.next;
+            }
+        }
+
+//        ListNode temp = head;
+//        while (temp != null) {
+//            System.out.println(temp.element);
+//            temp = temp.next;
+//        }
+        return flatHead;
+    }
+
+    @Override
+    public boolean isCircular(ListNode head) {
+        HashSet<Object> set = new HashSet<>();
+
+        if (head == null) {
+            return false;
+        }
+
+        ListNode current = head;
+
+        while (current.next != null) {
+            if (set.contains(current.next)) {
+                return current.next == head;
+            } else {
+                set.add(current);
+            }
+            current = current.next;
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean containsCycles(ListNode head) {
+        HashSet<Object> set = new HashSet<>();
+
+        if (head == null) {
+            return false;
+        }
+
+        ListNode current = head;
+
+        while (current.next != null) {
+            if (set.contains(current.next)) {
+                return true;
+            } else {
+                set.add(current);
+            }
+            current = current.next;
+        }
+
+        return false;
+    }
+
+    @Override
+    public ListNode sort(ListNode head, Comparator comparator) {
+
+        if (head == null && head.next == null) {
+            return head;
+        }
+
+
+
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ListNode map(ListNode head, ITransformation transformation) {
+
+        ListNode current = head;
+
+        while (current != null) {
+
+            current.element = transformation.transform(current.element);
+            current = current.next;
+        }
+
+        return head;
+    }
+
+    @Override
+    public Object reduce(ListNode head, IOperator operator, Object initial) {
+        ListNode current = head;
+        Object returnValue = initial;
+
+        while (current != null) {
+
+            returnValue = operator.operate(current.element, returnValue);
+            current = current.next;
+        }
+
+        return returnValue;
+    }
+
+}
---
+++ RecursiveListManipulator.java
@@ -0,0 +1,205 @@
+package impl;
+
+import java.util.Comparator;
+
+import common.InvalidIndexException;
+import common.ListNode;
+import interfaces.IListManipulator;
+import interfaces.IOperator;
+import interfaces.ITransformation;
+
+/**
+ * This class represents the recursive implementation of the IListManipulator interface.
+ *
+ */
+public class RecursiveListManipulator implements IListManipulator {
+
+    @Override
+    public int size(ListNode head) {
+        if (head == null) {
+            return 0;
+        }
+        if (head.next == null) {
+            return 1;
+        } else {
+            return size(head.next) + 1;
+        }
+    }
+
+    @Override
+    public boolean contains(ListNode head, Object element) {
+        if (head == null) {
+            return false;
+        } else if (head.element.equals(element)) {
+            return true;
+        }
+
+        return contains(head.next, element);
+    }
+
+    @Override
+    public int count(ListNode head, Object element) {
+        if (head == null) {
+            return 0;
+        } else if (head.element.equals(element)) {
+            return count(head.next, element) + 1;
+        }
+
+        return count(head.next, element);
+    }
+
+    @Override
+    public String convertToString(ListNode head) {
+        if (head == null) {
+            return "";
+        }
+        if (head.next == null) {
+            return head.element.toString();
+        } else {
+            return head.element.toString() + "," + convertToString(head.next);
+        }
+    }
+
+    @Override
+    public Object getFromFront(ListNode head, int n) throws InvalidIndexException {
+        if (head == null) {
+            throw new InvalidIndexException();
+        }
+        if (n == 0) {
+            return head.element;
+        } else {
+            return getFromFront(head.next, n - 1);
+        }
+    }
+
+    @Override
+    public Object getFromBack(ListNode head, int n) throws InvalidIndexException {
+        if (head == null) {
+            throw new InvalidIndexException();
+        }
+        if (size(head) - 1 == n) { //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!   HIGH COMPLEXITY
+            return head.element;
+        } else {
+            return getFromFront(head.next, n);
+        }
+    }
+
+    @Override
+    public boolean deepEquals(ListNode head1, ListNode head2) {
+
+        if (head1 == null && head2 == null) {
+            return true;
+        } else if (head1 == null || head2 == null) {
+            return false;
+        } else if (!head1.element.equals(head2.element)) {
+            return false;
+        } else {
+            return deepEquals(head1.next, head2.next);
+        }
+    }
+
+    @Override
+    public ListNode deepCopy(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        if (head.next == null) {
+            return new ListNode(head.element);
+        } else {
+            return new ListNode(head.element, deepCopy(head.next));
+        }
+    }
+
+    @Override
+    public boolean containsDuplicates(ListNode head) {
+
+        // TODO !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        if (head.next == null) {
+            return false;
+        } else {
+            return containsDuplicates(head.next);
+        }
+    }
+
+    @Override
+    public ListNode append(ListNode head1, ListNode head2) {
+        if (head2 == null) {
+            return head1;
+        }
+        if (head1 == null) {
+            return head2;
+        }
+        if (head1.next == null) {
+            return new ListNode(head1.element, head2);
+        } else {
+            return new ListNode(head1.element, append(head1.next, head2));
+        }
+    }
+
+    @Override
+    public ListNode flatten(ListNode head) { //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        if (head == null) {
+            return null;
+        }
+        if (head.next == null) {
+            System.out.println("!!!!");
+            return new ListNode(head.element, head);
+        }
+        if (head.element instanceof ListNode) {
+            if (((ListNode) head.element).next != null) {
+                System.out.println("!");
+                return new ListNode(((ListNode) head.element).element, flatten(((ListNode) head.element).next));
+            } else {
+                System.out.println("!!");
+                return new ListNode(((ListNode) head.element).element);
+            }
+        } else {
+            System.out.println("!!!");
+            return new ListNode(head.element, flatten(head.next));
+        }
+    }
+
+    @Override
+    public boolean isCircular(ListNode head) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean containsCycles(ListNode head) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public ListNode sort(ListNode head, Comparator comparator) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ListNode map(ListNode head, ITransformation transformation) {
+        if (head == null) {
+            return null;
+        }
+        if (head.next == null) {
+            return new ListNode(transformation.transform(head.element));
+        } else {
+            return new ListNode(transformation.transform(head.element), map(head.next, transformation));
+        }
+    }
+
+    @Override
+    public Object reduce(ListNode head, IOperator operator, Object initial) { //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        if (head == null) {
+            return null;
+        }
+        if (head.next == null) {
+            return operator.operate(initial, head.element);
+        } else {
+            //initial = operator.operate(initial, head.element);
+            return operator.operate(reduce(head.next, operator, initial), head.element);
+        }
+    }
+
+}
